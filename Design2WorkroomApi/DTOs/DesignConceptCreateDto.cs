4a29c858bdc4498d885eb47c56ced6aa93829ae1
wmaw <will@test.com>
2021-12-30 13:09:16 +0530

    Get data from API FormBody

--- Design2WorkroomApi/DTOs/DesignConceptCreateDto.cs
+++ Design2WorkroomApi/DTOs/DesignConceptCreateDto.cs
@@ -2,5 +2,10 @@
 {
     public class DesignConceptCreateDto
     {
+        public string ImageUrl { get; set; }
+
+        public Guid DesignerId { get; set; }
+
+        public Guid ClientId { get; set; }
     }
 }
