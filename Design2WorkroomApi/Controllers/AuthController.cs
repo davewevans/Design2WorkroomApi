cbf1d779ae3fdf4675500635cf8ca1ce1e700fb9
Freelancingonupwork <freelancingonupwork@gmail.com>
2022-01-20 16:08:30 +0530

    Update AuthController.cs

--- Design2WorkroomApi/Controllers/AuthController.cs
+++ Design2WorkroomApi/Controllers/AuthController.cs
@@ -191,7 +191,7 @@ namespace Design2WorkroomApi.Controllers
                     var appRoles_Value = AppUserRole.Designer.ToString();// (appRoles == null || !appRoles.Any()) ? null : string.Join(' ', appRoles);
 
                     //return GetContinueApiResponse("GetAppRoles-Succeeded", "Your app roles were successfully determined.", appRoles_Value, createResult.UserId);
-                    return GetBlockPageApiResponse("GetAppRoles-InternalError", "Something went wrong...." + body.ToString());
+                    return GetValidationErrorApiResponse("GetAppRoles-InternalError", "Something went wrong...." + body.ToString());
                 }
                 
 
@@ -199,7 +199,7 @@ namespace Design2WorkroomApi.Controllers
                 var appRoles = await _appRolesProvider.GetAppRolesAsync(email, objectId);
                 if(appRoles.IsSuccess)
                 {
-                    return GetBlockPageApiResponse("GetAppRoles-InternalError", "Something went wrong...." + body.ToString());
+                    return GetValidationErrorApiResponse("GetAppRoles-InternalError", "Something went wrong...." + body.ToString());
                     //return GetContinueApiResponse("GetAppRoles-Succeeded", "Your app roles were successfully determined.", appRoles.AppUserRole, appRoles.UserId);
                 }
                 else
