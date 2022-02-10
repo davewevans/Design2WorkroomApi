55428d77b4f5a4235717c80fa73742b9eded6d1e
upworkfreelancer <freelancingonupwork@gmail.com>
2022-02-10 18:33:26 +0530

    Update AuthController.cs

--- Design2WorkroomApi/Controllers/AuthController.cs
+++ Design2WorkroomApi/Controllers/AuthController.cs
@@ -242,7 +242,110 @@ namespace Design2WorkroomApi.Controllers
                 _logger.LogError(exc, "Error while processing request body: " + exc.ToString());
                 return GetValidationErrorApiResponse("GetAppRoles-InternalError", exc.ToString());
             }
-        }       
+        }
+
+        [HttpPost("GetAppUserRoles")]
+        public async Task<IActionResult> GetAppUserRoles([FromBody] JsonElement body)
+        {
+            // Azure AD B2C calls into this API when a user is attempting to sign in.
+            // We expect a JSON object in the HTTP request which contains the input claims.
+            try
+            {
+                _logger.LogInformation("App roles are being requested.");
+                _logger.LogTrace(JsonSerializer.Serialize(body, new JsonSerializerOptions { WriteIndented = true }));
+
+                // Log the incoming request body.
+                _logger.LogInformation("Request body:");
+                _logger.LogInformation(JsonSerializer.Serialize(body, new JsonSerializerOptions { WriteIndented = true }));
+
+                // Get the object id of the user that is signing in.
+                var objectId = body.GetProperty("objectId").GetString();
+
+                // Get the client id of the app that the user is signing in to.
+                var clientId = body.GetProperty("client_id").GetString();
+                var email = body.GetProperty("email").GetString();
+                var firstName = "";
+                if (body.TryGetProperty("givenName", out JsonElement jsonElement5))
+                {
+                    firstName = body.GetProperty("givenName").GetString();
+                }
+                var lastName = "";
+                if (body.TryGetProperty("surname", out JsonElement jsonElement4))
+                {
+                    lastName = body.GetProperty("surname").GetString();
+                }
+                //var postalCode = body.GetProperty("postalCode").GetString();
+                var city = "";
+                if (body.TryGetProperty("city", out JsonElement jsonElement3))
+                {
+                    city = body.GetProperty("city").GetString();
+                }
+
+                var state = "";
+                if (body.TryGetProperty("state", out JsonElement jsonElement1))
+                {
+                    state = body.GetProperty("state").GetString();
+                }
+                var country = "";
+                if (body.TryGetProperty("country", out JsonElement jsonElement2))
+                {
+                    country = body.GetProperty("country").GetString();
+                }
+                return GetValidationErrorApiResponse("GetAppRoles-InternalError", "$$$$ Response : " + body);
+                var existsResult = await _designerRepo.DesignerExistsAsync(email);
+                //if(existsResult.Exists)
+                //{
+                //    var getExistsUSer = await _designerRepo.GetDesignerByEmailAsync(email);
+                //    if(getExistsUSer.Designer != null)
+                //    {
+                //        getExistsUSer.Designer.B2CObjectId = objectId;
+                //    }
+
+                //}
+                if (!existsResult.Exists)
+                {
+                    var designer = new DesignerModel(email, objectId)
+                    {
+                        AppUserRole = AppUserRole.Designer,
+                        CreatedAt = DateTime.UtcNow,
+                        Profile = new ProfileModel(email, firstName, lastName, null, null, null, null, null, null, null, city, state, country)
+                        {
+                            CreatedAt = DateTime.UtcNow
+                        }
+                    };
+
+                    var createResult = await _designerRepo.CreateDesignerAsync(designer);
+                    var appRoles_Value = AppUserRole.Designer.ToString();// (appRoles == null || !appRoles.Any()) ? null : string.Join(' ', appRoles);
+
+                    return GetContinueApiResponse("GetAppRoles-Succeeded", "Your app roles were successfully determined.", appRoles_Value, createResult.UserId, firstName + " " + lastName);
+                    //return GetValidationErrorApiResponse("GetAppRoles-InternalError", "Something went wrong...." + body.ToString());
+                }
+
+                //return GetValidationErrorApiResponse("GetAppRoles-InternalError", "$$$$ User Exist Response : " + body);
+
+                // Retrieve the app roles assigned to the user for the requested application.
+                var appRoles = await _appRolesProvider.GetAppRolesAsync(email, objectId);
+                if (appRoles.IsSuccess)
+                {
+                    //return GetValidationErrorApiResponse("GetAppRoles-InternalError", "Something went wrong...." + body.ToString() + "$$appRoles : " + appRoles.AppUserRole + "$$UserId : " + appRoles.UserId + "Name : " + firstName + " " + lastName);
+                    return GetContinueApiResponse("GetAppRoles-Succeeded", "Your app roles were successfully determined.", appRoles.AppUserRole, appRoles.UserId, firstName + " " + lastName);
+                }
+                else
+                {
+                    return GetValidationErrorApiResponse("GetAppRoles-InternalError", appRoles.ErrorMessage + "$$$$ Response : " + body);
+                }
+                // Custom user attributes in Azure AD B2C cannot be collections, so we emit them
+                // into a single claim value separated with spaces.
+                //var appRolesValue = AppUserRole.Designer.ToString();// (appRoles == null || !appRoles.Any()) ? null : string.Join(' ', appRoles);
+
+                //return GetBlockPageApiResponse("GetAppRoles-InternalError", "An error occurred while determining your app roles, please try again later.");
+            }
+            catch (Exception exc)
+            {
+                _logger.LogError(exc, "Error while processing request body: " + exc.ToString());
+                return GetValidationErrorApiResponse("GetAppRoles-InternalError", exc.ToString());
+            }
+        }
 
         private IActionResult GetContinueApiResponse(string code, string userMessage, string appRoles, string UserId = "", string userName = "")
         {
